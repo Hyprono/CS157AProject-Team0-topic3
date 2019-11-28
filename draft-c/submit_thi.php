e433049714d632477e21947f5afd74923ed78714
Alexander McNulty <Alexander.mcnulty92@gmail.com>
2019-11-27 20:12:53 -0800

    full layout for thi, need scoreing in tfi

---
+++ draft-c/submit_thi.php
@@ -0,0 +1,97 @@
+  
+<?php
+	require_once "thi.php";
+	require_once "login.php";
+	require_once "main.php";
+	session_start();
+	$newID = $_SESSION['newID'];
+	$qArray = array($_POST['q1'],$_POST['q2'],$_POST['q3'],$_POST['q4'],$_POST['q5'],
+$_POST['q6'],$_POST['q7'],$_POST['q8'],$_POST['q9'],$_POST['q10'],$_POST['q11'],$_POST['q12'],
+$_POST['q13'],$_POST['q14'],$_POST['q15'],$_POST['q16'],$_POST['q17'],$_POST['q18'],$_POST['q19'],$_POST['q20'],$_POST['q21'],$_POST['q22'],$_POST['q23'],$_POST['q24'],$_POST['q25']);
+	
+	$typeArray = array("F","F","E","F","C","E","F","C","F","E",
+"C","F","F","E","F","E","E","F","C","F","E","E","C","F","E");	
+	$sc_t = 0;
+	$sc_f = 0;
+	$sc_e = 0;
+	$sc_c = 0;
+	$complete = true;
+	$uncompleted = array();
+	for($i = 1; $i <= count($qArray); $i++)
+	{
+		if($qArray[$i-1] == null)
+		{	
+			$complete = false;
+			$uncompleted[] = $i;
+		}
+		else
+		{
+			$sc_t += $qArray[$i-1];
+			if($typeArray[$i-1] == "F")
+			{
+				$sc_f = $sc_f + $qArray[$i-1];
+			}
+			if($typeArray[$i-1] == "C")
+			{
+				$sc_c = $sc_c + $qArray[$i-1];
+			}
+			if($typeArray[$i-1] == "E")
+			{
+				$sc_e = $sc_e + $qArray[$i-1];
+			}
+		}
+	}
+	if(count($uncompleted) != 0)
+	{
+		echo "<br/><br/><br/><div class='center' style='padding:20px;box-shadow:2px 2px 2px 4px #ccc'><h3>Hint:</h3><span>
+There are uncomplete questions.Please answer these questions before resubmitting for an accurate result. Your unfinished questions are: </span><div class=center>";
+		$string = "";
+		for($i = 0; $i<count($uncompleted); $i++)
+		{
+			$string = $string."$uncompleted[$i]";
+			if($i != count($uncompleted)-1)
+			{
+				$string = $string.", ";
+ 			}
+		}
+		echo $string;
+		die(print("</div></div><br/><br/><br/><br/><br/><br/>"));
+	}
+	echo <<<_END
+		<html>
+			<head>
+				<title>Form Upload</title>
+			</head>
+			<body>
+			<br/><br/><br/>
+			<div class='center' style='padding:20px;box-shadow:2px 2px 2px 4px #ccc'>
+				<h3>Results:</h3>
+				<div id="evaluation">
+					<b>The sum of all your responses is your THI score>>></b>
+					<textarea id="score" rows="1" readonly> $sc_t </textarea>
+				</div>
+				<div id="scale">
+					0-16: Slight or no handicap (Grade 1)</br>
+					18-36: Mild handicap (Grade 2) </br>
+					38-56: Moderate handicap (Grade 3) </br>
+					58-76: Severe handicap (Grade 4) </br>
+					78-100: Catastrophic handicap (Grade 5)
+				</div>
+			</div>	
+			<br/><br/><br/><br/>
+			</body>
+		</html>
+_END;
+	$conn = new mysqli($host_name, $user_name, $password, $database);
+	if($conn->connect_error) die(mysql_fatal_error("Your user info is either incorrect or not in the system."));
+	//currently has 25 ? for the q; will need to add more
+	$addEntry = $conn->prepare('INSERT INTO thi VALUES(?,?,?,?,?,?,?,?,?,?,
+		?,?,?,?,?,?,?,?,?,?,
+		?,?,?,?,?,?,?,?,?,?)');
+	$addEntry->bind_param('iiiiiiiiiiiiiiiiiiiiiiiiiiiiii',$sc_t,$sc_f,$sc_e,$sc_c,$_POST['q1'],$_POST['q2'],$_POST['q3'],$_POST['q4'],$_POST['q5'],
+$_POST['q6'],$_POST['q7'],$_POST['q8'],$_POST['q9'],$_POST['q10'],$_POST['q11'],$_POST['q12'],
+$_POST['q13'],$_POST['q14'],$_POST['q15'],$_POST['q16'],$_POST['q17'],$_POST['q18'],$_POST['q19'],$_POST['q20'],$_POST['q21'],$_POST['q22'],$_POST['q23'],$_POST['q24'],$_POST['q25'],$newID);
+	$addEntry->execute();
+	$addEntry->close();
+	$conn->close();
+?>
---
+++ draft-c/thi.php
@@ -0,0 +1,304 @@
+<?php
+
+	require_once "main.php";
+	echo <<<_END
+	<!DOCTYPE HTML>
+	<html>  
+	<head>
+		<title> THI </title>
+		<link href="./thi_css.css" rel="stylesheet" type="text/css">
+		<style>
+			td div{ padding:10px; max-width: 600px; min-width:600px }
+		</style>
+	</head>
+	<body>
+	</br>
+	</br>
+	</br>
+
+	<form method="post" action="submit_thi.php" enctype="multipart/form-data">
+	<div id="page" class="center" style="box-shadow:2px 2px 2px 4px #ccc; width:660px;">
+		<div>
+				<div>
+					<h1>Tinnitus Handicap Inventory (THI)</h1>
+				</div>
+				<blob style="border:1px solid black;width:600px;display:block;padding:10px;">
+					<i>This form is for <u>informational purposes only</u> and should not take the place of consulation and evaluation by a healthcare professional</i>
+				</blob>
+			
+			<div id="name_and_date">
+				<div class="left">
+					Your Name:  <input type="text" size="35" name="name">
+				</div>
+				<div class="right">
+					Date: <input type="text" size="20" name="date">
+				</div>
+			</div>
+			<div id="instructions">
+				<B>Instructions:</B> The purpose of this questionnaire is to identify, quantify, and evaluate the difficulties that you may be experiencing because of tinnitus. When you have answered all the questions, add up your total score, based on the values of each response.
+			</div>
+		</div>
+
+		<table id="questions" style='width:660px;''>
+			<tr class="lightgray">
+				<td class="row">
+					<div>1. Because of tinnitus, it is difficult for you to concentrate?</div>
+						<div class="right">
+							<input type="radio" name="q1" value="4">Yes (4)
+							<input type="radio" name="q1" value="2">Sometimes (2)
+							<input type="radio" name="q1" value="0">Never (0)
+						</div>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">2. Does the loudness of your tinnitus make it difficult for you to hear people?</div>
+					<div class="right">
+						<input type="radio" name="q2" value="4">Yes (4)
+						<input type="radio" name="q2" value="2">Sometimes (2)
+						<input type="radio" name="q2" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td>
+					<div class="row left">3. Does your tinnitus make you angry?</div>
+					<div class="row right">
+						<input type="radio" name="q3" value="4">Yes (4)
+						<input type="radio" name="q3" value="2">Sometimes (2)
+						<input type="radio" name="q3" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">4. Does your tinnitus make you feel confused?</div>
+					<div class="right">
+						<input type="radio" name="q4" value="4">Yes (4)
+						<input type="radio" name="q4" value="2">Sometimes (2)
+						<input type="radio" name="q4" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">5. Because of your tinnitus, do you feel desperate?</div>
+					<div class="right">
+						<input type="radio" name="q5" value="4">Yes (4)
+						<input type="radio" name="q5" value="2">Sometimes (2)
+						<input type="radio" name="q5" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">6. Do you complain a great deal about your tinnitus?</div>
+					<div class="right">
+						<input type="radio" name="q6" value="4">Yes (4)
+						<input type="radio" name="q6" value="2">Sometimes (2)
+						<input type="radio" name="q6" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">7. Because of your tinnitus, do you have trouble falling asleep at night?</div>
+					<div class="right">
+						<input type="radio" name="q7" value="4">Yes (4)
+						<input type="radio" name="q7" value="2">Sometimes (2)
+						<input type="radio" name="q7" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">8. Do you feel as though you cannot escape your tinnitus?</div>
+					<div class="right">
+						<input type="radio" name="q8" value="4">Yes (4)
+						<input type="radio" name="q8" value="2">Sometimes (2)
+						<input type="radio" name="q8" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">9. Does your tinnitus interfere with your ability to enjoy your socail activites (such as going out to dinner, to the movies)?</div>
+					<div class="right">
+						<input type="radio" name="q9" value="4">Yes (4)
+						<input type="radio" name="q9" value="2">Sometimes (2)
+						<input type="radio" name="q9" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">10. Because of your tinnitus, do you feel frustrated?</div>
+					<div class="right">
+						<input type="radio" name="q10" value="4">Yes (4)
+						<input type="radio" name="q10" value="2">Sometimes (2)
+						<input type="radio" name="q10" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">11. Because of your tinnitus, do you feel that you have a terrible disease?</div>
+					<div class="right">
+						<input type="radio" name="q11" value="4">Yes (4)
+						<input type="radio" name="q11" value="2">Sometimes (2)
+						<input type="radio" name="q11" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">12. Does your tinnitus make itdifficult for you to enjoy life?</div>
+					<div class="right">
+						<input type="radio" name="q12" value="4">Yes (4)
+						<input type="radio" name="q12" value="2">Sometimes (2)
+						<input type="radio" name="q12" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">13. Does your tinnitus interfere with your job or household responsibilities?</div>
+					<div class="right">
+						<input type="radio" name="q13" value="4">Yes (4)
+						<input type="radio" name="q13" value="2">Sometimes (2)
+						<input type="radio" name="q13" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">14. Because of your tinnitus, do you find that you are often irritable?</div>
+					<div class="right">
+						<input type="radio" name="q14" value="4">Yes (4)
+						<input type="radio" name="q14" value="2">Sometimes (2)
+						<input type="radio" name="q14" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">15. Because of your tinnitus, is it difficult for you to read?</div>
+					<div class="right">
+						<input type="radio" name="q15" value="4">Yes (4)
+						<input type="radio" name="q15" value="2">Sometimes (2)
+						<input type="radio" name="q15" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">16. Does your tinnitus make you upset?</div>
+					<div class="right">
+						<input type="radio" name="q16" value="4">Yes (4)
+						<input type="radio" name="q16" value="2">Sometimes (2)
+						<input type="radio" name="q16" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">17. Do you feel that your tinnitus problem has placed stress on your reslationships with members of your family and friends?</div>
+					<div class="right">
+						<input type="radio" name="q17" value="4">Yes (4)
+						<input type="radio" name="q17" value="2">Sometimes (2)
+						<input type="radio" name="q17" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">18. Do you find it difficult to focus your attention away from your tinnitus and on other things?</div>
+					<div class="right">
+						<input type="radio" name="q18" value="4">Yes (4)
+						<input type="radio" name="q18" value="2">Sometimes (2)
+						<input type="radio" name="q18" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">19. Do you feel that you have no control over your tinnitus?</div>
+					<div class="right">
+						<input type="radio" name="q19" value="4">Yes (4)
+						<input type="radio" name="q19" value="2">Sometimes (2)
+						<input type="radio" name="q19" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">20. Because of your tinnitus, do you often feel tired?</div>
+					<div class="right">
+						<input type="radio" name="q20" value="4">Yes (4)
+						<input type="radio" name="q20" value="2">Sometimes (2)
+						<input type="radio" name="q20" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">21. Because of your tinnitus, do you feel depressed?</div>
+					<div class="right">
+						<input type="radio" name="q21" value="4">Yes (4)
+						<input type="radio" name="q21" value="2">Sometimes (2)
+						<input type="radio" name="q21" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">22. Does your tinnitus make you feel anxious?</div>
+					<div class="right">
+						<input type="radio" name="q22" value="4">Yes (4)
+						<input type="radio" name="q22" value="2">Sometimes (2)
+						<input type="radio" name="q22" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">23. Do you feel that you can no longer cope with your tinnitus?</div>
+					<div class="right">
+						<input type="radio" name="q23" value="4">Yes (4)
+						<input type="radio" name="q23" value="2">Sometimes (2)
+						<input type="radio" name="q23" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td class="row">
+					<div class="left">24. Does your tinnitus get worse when you are under stress?</div>
+					<div class="right">
+						<input type="radio" name="q24" value="4">Yes (4)
+						<input type="radio" name="q24" value="2">Sometimes (2)
+						<input type="radio" name="q24" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			<tr class="lightgray">
+				<td class="row">
+					<div class="left">25. Does your tinnitus make you feel insecure?</div>
+					<div class="right">
+						<input type="radio" name="q25" value="4">Yes (4)
+						<input type="radio" name="q25" value="2">Sometimes (2)
+						<input type="radio" name="q25" value="0">Never (0)
+					</div>
+				</td>
+			</tr>
+			</table>
+			<div class="center">
+				<button type="submit" style="margin:20px 10px;width:600px;font-size:20px;" name="thi_form">Submit</button>
+			</div>
+	</div>
+	</form>
+	</body>
+	</html>
+	';
+_END
+?>
